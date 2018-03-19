2716a197c212ba08685d440b9fa81873e6975a6d
agebretinsai1 <agebretinsai1@nsmb208-xx.csudh.edu>
2018-03-19 16:38:05 -0700

    Version 1

---
+++ room1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+void  main(void)
+{
+        printf("This is Room 1\n");
+}
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --                                                  7,2           All
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --                                                  7,2           All
+
