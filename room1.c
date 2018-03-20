b69b0246ccf34f44d7dcfc35cccd659da31f7728
agebretinsai1 <agebretinsai1@nsmb208-xx.csudh.edu>
2018-03-19 17:16:27 -0700

    Version3

--- room1.c
+++ room1.c
@@ -5,7 +5,7 @@ int main (void)
 	int choice = 5;
 	
 	printf("Hello and welcome to our RPG. You are in room 1\n");
-	printf("");
+	printf("This will be a fun game");
 }	                                                  
 ~                                                                               
 ~                                                                               
